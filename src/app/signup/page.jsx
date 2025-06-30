dfadd7fa284198c9220c4288c8b1ca9c5754786f
aditya <xyz059011@gmail.com>
2025-06-30 15:43:29 +0000

    buffering on loading page itself

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -53,7 +53,7 @@ export default function SignupPage() {
     }
   };
 
-  if (loading || user) {
+  if (loading) {
     return (
       <div className="flex h-screen w-full items-center justify-center">
         <Loader2 className="h-16 w-16 animate-spin text-primary" />
