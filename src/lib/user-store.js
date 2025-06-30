e2f2b44c3bce9a0aea055555eb125615722666d3
aditya <xyz059011@gmail.com>
2025-06-30 14:58:49 +0000

    when the app closes we have to create id password again . why is it so ,

--- src/lib/user-store.js
+++ src/lib/user-store.js
@@ -1,26 +1,39 @@
 
 import bcrypt from 'bcryptjs';
+import mongoose from 'mongoose';
+import connectDB from './mongoose';
 
-// In-memory store for users. In a real app, this would be a database.
-// To prevent the store from being cleared on hot-reloads in development,
-// we attach it to the global object. This is a common pattern.
-const globalForUsers = globalThis;
-const users = globalForUsers.users || (globalForUsers.users = []);
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
+// Avoid model recompilation in Next.js hot-reload environments
+const User = mongoose.models.User || mongoose.model('User', UserSchema);
 
 export async function findUserByUsername(username) {
-  return users.find(user => user.username === username);
+  await connectDB();
+  return User.findOne({ username });
 }
 
 export async function createUser({ username, password }) {
+  await connectDB();
   const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = {
-    id: Date.now().toString(),
+  const newUser = new User({
     username,
     password: hashedPassword,
-  };
-  users.push(newUser);
-  return newUser;
+  });
+  await newUser.save();
+  // Return a plain object to avoid passing non-serializable data
+  return { id: newUser._id.toString(), username: newUser.username };
 }
 
 export async function verifyPassword(password, hashedPassword) {
